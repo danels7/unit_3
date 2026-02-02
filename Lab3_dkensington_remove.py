4c147dbb799fd31f6865b642c4c5b32c212fc02e
danels7 <danels7@outlook.com>
2026-02-01 20:41:21 -0500

    add docstring to Lab3_dkensington_remove.py

---
+++ Lab3_dkensington_remove.py
@@ -0,0 +1,8 @@
+"""
+Camping List
+Daniel Kensington
+Remove elements from list of camping supplies
+List 'items' defined in Lab3_dkensington_list.py and\
+imported from Lab3_dkensington_replace.py
+2/1/2026
+"""
