190848704532624ad9e1a08e0e88c9e3914e633d
danels7 <danels7@outlook.com>
2026-02-01 20:15:36 -0500

    add docstring to Lab3_dkensington_replace.py

---
+++ Lab3_dkensington_replace.py
@@ -0,0 +1,10 @@
+"""
+Camping List
+Daniel Kensington
+Replace elements in list of camping supplies
+List 'items' defined in Lab3_dkensington_list.py and\
+imported from Lab3_dkensington_list.py
+2/1/2026
+"""
+
+
