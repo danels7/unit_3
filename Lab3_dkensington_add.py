54a3eedfafe615973c7329ce74b1b33be6b868b6
danels7 <danels7@outlook.com>
2026-02-01 20:04:22 -0500

    make list of more items and append them to 'items'

--- Lab3_dkensington_add.py
+++ Lab3_dkensington_add.py
@@ -5,3 +5,19 @@ Add to list of camping supplies
 List 'items' defined in Lab3_dkensington_list.py
 2/1/2026
 """
+
+
+from Lab3_dkensington_list import items
+
+
+new_items: list[str] = [
+    "sunscreen",
+    "bug spray",
+    "pocket knife",
+    "chair",
+    "map"
+]
+
+
+for i in new_items:
+    items.append(i)
