287a768ab2ae7462561a65a5b0615cbbec543abc
danels7 <danels7@outlook.com>
2026-02-01 20:48:53 -0500

    remove binoculars

--- Lab3_dkensington_remove.py
+++ Lab3_dkensington_remove.py
@@ -6,3 +6,9 @@ List 'items' defined in Lab3_dkensington_list.py and\
 imported from Lab3_dkensington_replace.py
 2/1/2026
 """
+
+
+from Lab3_dkensington_replace import items
+
+
+items.remove("binoculars")
