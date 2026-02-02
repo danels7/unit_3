9e5fbca60765d486cfb65bead41ec85cf4c6a567
danels7 <danels7@outlook.com>
2026-02-01 19:42:46 -0500

    make list of items

--- Lab3_dkensington_list.py
+++ Lab3_dkensington_list.py
@@ -4,3 +4,19 @@ Daniel Kensington
 List of camping supplies
 2/1/2026
 """
+
+
+items: list[str] = [
+    "tent",
+    "sleeping bag",
+    "air matress",
+    "backpack",
+    "food",
+    "water",
+    "extra clothes",
+    "lighter",
+    "flashlight",
+    "first aid kit"
+]
+
+
